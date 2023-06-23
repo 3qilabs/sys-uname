f91565897153be0ea29427254dff0d50fd6ba5e6
vinayak3qilabs <vinayak@3qilabs.com>
2023-06-23 11:37:42 +0530

    Vinayak[AWE-3264] win32ole gem gives issue so comment gem and modified code.

--- lib/sys/windows/uname.rb
+++ lib/sys/windows/uname.rb
@@ -2,7 +2,7 @@
 
 require 'socket'
 require 'time'
-require 'win32ole'
+#require 'win32ole'
 
 # The Sys module provides a namespace only.
 module Sys
@@ -81,29 +81,50 @@ module Sys
     # including it as part of the connection.
     #
     def self.version(host = Socket.gethostname)
-      cs = "winmgmts://#{host}/root/cimv2"
       begin
-        wmi = WIN32OLE.connect(cs)
-      rescue WIN32OLERuntimeError => err
+        host_os = RbConfig::CONFIG['host_os']
+      rescue Exception => err
         raise Error, err
       else
-        ole = wmi.InstancesOf('Win32_OperatingSystem').ItemIndex(0)
-        str = "#{ole.Version} #{ole.BuildNumber}-"
-        "#{str}#{ole.ServicePackMajorVersion}"
+        case host_os
+        when /darwin|mac os/
+          # macOS version
+          `sw_vers -productVersion`.strip
+        when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+          # Windows version
+          `ver`.strip
+        when /linux/
+          # Linux version
+          `lsb_release -r -s`.strip
+        when /solaris|bsd/
+          # Solaris or BSD version
+          `uname -r`.strip
+        else
+          'Unknown'
+        end
       end
     end
 
     # Returns the operating system name, e.g. "Microsoft Windows XP Home"
     #
     def self.sysname(host = Socket.gethostname)
-      cs = 'winmgmts:{impersonationLevel=impersonate,(security)}'
-      cs += "//#{host}/root/cimv2"
       begin
-        wmi = WIN32OLE.connect(cs)
-      rescue WIN32OLERuntimeError => err
+        host_os = RbConfig::CONFIG['host_os']
+      rescue Exception => err
         raise Error, err
       else
-        wmi.InstancesOf('Win32_OperatingSystem').ItemIndex(0).Caption.strip
+        case host_os
+        when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+          'Windows'
+        when /darwin|mac os/
+          'Mac OS'
+        when /linux/
+          'Linux'
+        when /solaris|bsd/
+          'Unix'
+        else
+          'Unknown'
+        end
       end
     end
 
@@ -111,43 +132,36 @@ module Sys
     # same as the system's hostname.
     #
     def self.nodename(host = Socket.gethostname)
-      cs = 'winmgmts:{impersonationLevel=impersonate,(security)}'
-      cs += "//#{host}/root/cimv2"
       begin
-        wmi = WIN32OLE.connect(cs)
-      rescue WIN32OLERuntimeError => err
+        host_os = RbConfig::CONFIG['host_os']
+      rescue Exception => err
         raise Error, err
       else
-        wmi.InstancesOf('Win32_OperatingSystem').ItemIndex(0).CSName
+        case host_os
+        when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+          'Windows'
+        when /darwin|mac os/
+          'Mac OS'
+        when /linux/
+          'Linux'
+        when /solaris|bsd/
+          'Unix'
+        else
+          'Unknown'
+        end
       end
     end
 
     # Returns the CPU architecture, e.g. "x86"
     #
     def self.architecture(cpu_num = 0, host = Socket.gethostname)
-      cs = 'winmgmts:{impersonationLevel=impersonate,(security)}'
-      cs += "//#{host}/root/cimv2:Win32_Processor='cpu#{cpu_num}'"
+
       begin
-        wmi = WIN32OLE.connect(cs)
-      rescue WIN32OLERuntimeError => err
+        cpu_architecture = RbConfig::CONFIG['target_cpu']
+      rescue Exception => err
         raise Error, err
       else
-        case wmi.Architecture
-          when 0
-            'x86'
-          when 1
-            'mips'
-          when 2
-            'alpha'
-          when 3
-            'powerpc'
-          when 6
-            'ia64'
-          when 9
-            'x86_64'
-          else
-            'unknown'
-        end
+        cpu_architecture
       end
     end
 
@@ -158,247 +172,42 @@ module Sys
     # appears that MS doesn't necessarily patch this, either.
     #
     def self.machine(cpu_num = 0, host = Socket.gethostname)
-      cs = 'winmgmts:{impersonationLevel=impersonate,(security)}'
-      cs += "//#{host}/root/cimv2:Win32_Processor='cpu#{cpu_num}'"
+
       begin
-        wmi = WIN32OLE.connect(cs)
-      rescue WIN32OLERuntimeError => err
+        cpu_architecture = RbConfig::CONFIG['target_cpu']
+      rescue Exception => err
         raise Error, err
       else
         # Convert a family number into the equivalent string
-        case wmi.Family
-          when 1
-            'Other'
-          when 3
-            '8086'
-          when 4
-            '80286'
-          when 5
-            '80386'
-          when 6
-            '80486'
-          when 7
-            '8087'
-          when 8
-            '80287'
-          when 9
-            '80387'
-          when 10
-            '80487'
-          when 11
-            'Pentium brand'
-          when 12
-            'Pentium Pro'
-          when 13
-            'Pentium II'
-          when 14
-            'Pentium processor with MMX technology'
-          when 15
-            'Celeron'
-          when 16
-            'Pentium II Xeon'
-          when 17
-            'Pentium III'
-          when 18
-            'M1 Family'
-          when 19
-            'M2 Family'
-          when 24
-            'K5 Family'
-          when 25
-            'K6 Family'
-          when 26
-            'K6-2'
-          when 27
-            'K6-3'
-          when 28
-            'AMD Athlon Processor Family'
-          when 29
-            'AMD Duron Processor'
-          when 30
-            'AMD2900 Family'
-          when 31
-            'K6-2+'
-          when 32
-            'Power PC Family'
-          when 33
-            'Power PC 601'
-          when 34
-            'Power PC 603'
-          when 35
-            'Power PC 603+'
-          when 36
-            'Power PC 604'
-          when 37
-            'Power PC 620'
-          when 38
-            'Power PC X704'
-          when 39
-            'Power PC 750'
-          when 48
-            'Alpha Family'
-          when 49
-            'Alpha 21064'
-          when 50
-            'Alpha 21066'
-          when 51
-            'Alpha 21164'
-          when 52
-            'Alpha 21164PC'
-          when 53
-            'Alpha 21164a'
-          when 54
-            'Alpha 21264'
-          when 55
-            'Alpha 21364'
-          when 64
-            'MIPS Family'
-          when 65
-            'MIPS R4000'
-          when 66
-            'MIPS R4200'
-          when 67
-            'MIPS R4400'
-          when 68
-            'MIPS R4600'
-          when 69
-            'MIPS R10000'
-          when 80
-            'SPARC Family'
-          when 81
-            'SuperSPARC'
-          when 82
-            'microSPARC II'
-          when 83
-            'microSPARC IIep'
-          when 84
-            'UltraSPARC'
-          when 85
-            'UltraSPARC II'
-          when 86
-            'UltraSPARC IIi'
-          when 87
-            'UltraSPARC III'
-          when 88
-            'UltraSPARC IIIi'
-          when 96
-            '68040'
-          when 97
-            '68xxx Family'
-          when 98
-            '68000'
-          when 99
-            '68010'
-          when 100
-            '68020'
-          when 101
-            '68030'
-          when 112
-            'Hobbit Family'
-          when 120
-            'Crusoe TM5000 Family'
-          when 121
-            'Crusoe TM3000 Family'
-          when 122
-            'Efficeon TM8000 Family'
-          when 128
-            'Weitek'
-          when 130
-            'Itanium Processor'
-          when 131
-            'AMD Athlon 64 Processor Family'
-          when 132
-            'AMD Opteron Processor Family'
-          when 144
-            'PA-RISC Family'
-          when 145
-            'PA-RISC 8500'
-          when 146
-            'PA-RISC 8000'
-          when 147
-            'PA-RISC 7300LC'
-          when 148
-            'PA-RISC 7200'
-          when 149
-            'PA-RISC 7100LC'
-          when 150
-            'PA-RISC 7100'
-          when 160
-            'V30 Family'
-          when 176
-            'Pentium III Xeon'
-          when 177
-            'Pentium III Processor with Intel SpeedStep Technology'
-          when 178
-            'Pentium 4'
-          when 179
-            'Intel Xeon'
-          when 180
-            'AS400 Family'
-          when 181
-            'Intel Xeon processor MP'
-          when 182
-            'AMD AthlonXP Family'
-          when 183
-            'AMD AthlonMP Family'
-          when 184
-            'Intel Itanium 2'
-          when 185
-            'AMD Opteron Family'
-          when 190
-            'K7'
-          when 198
-            'Intel Core i7-2760QM'
-          when 200
-            'IBM390 Family'
-          when 201
-            'G4'
-          when 202
-            'G5'
-          when 203
-            'G6'
-          when 204
-            'z/Architecture Base'
-          when 250
-            'i860'
-          when 251
-            'i960'
-          when 260
-            'SH-3'
-          when 261
-            'SH-4'
-          when 280
-            'ARM'
-          when 281
-            'StrongARM'
-          when 300
-            '6x86'
-          when 301
-            'MediaGX'
-          when 302
-            'MII'
-          when 320
-            'WinChip'
-          when 350
-            'DSP'
-          when 500
-            'Video Processor'
-          else
-            'Unknown'
-        end
+        cpu_architecture
       end
     end
 
     # Returns the release number, e.g. 5.1.2600.
     #
     def self.release(host = Socket.gethostname)
-      cs = "winmgmts://#{host}/root/cimv2"
+
       begin
-        wmi = WIN32OLE.connect(cs)
-      rescue WIN32OLERuntimeError => err
+        host_os = RbConfig::CONFIG['host_os']
+      rescue Exception => err
         raise Error, err
       else
-        wmi.InstancesOf('Win32_OperatingSystem').ItemIndex(0).Version
+        case host_os
+        when /darwin|mac os/
+          # macOS version
+          `sw_vers -productVersion`.strip
+        when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+          # Windows version
+          `ver`.strip
+        when /linux/
+          # Linux version
+          `lsb_release -r -s`.strip
+        when /solaris|bsd/
+          # Solaris or BSD version
+          `uname -r`.strip
+        else
+          'Unknown'
+        end
       end
     end
 
@@ -407,71 +216,17 @@ module Sys
     # Please see the MSDN documentation for what each of these fields mean.
     #
     def self.uname(host = Socket.gethostname)
-      cs = "winmgmts://#{host}/root/cimv2"
       begin
-        wmi = WIN32OLE.connect(cs)
-      rescue WIN32OLERuntimeError => err
+        host_os = RbConfig::CONFIG['host_os']
+        cpu_architecture = RbConfig::CONFIG['target_cpu']
+
+      rescue Exception => err
         raise Error, err
       else
-        os = wmi.InstancesOf('Win32_OperatingSystem').ItemIndex(0)
-
         UnameStruct.new(
-          os.BootDevice,
-          os.BuildNumber,
-          os.BuildType,
-          os.Caption,
-          os.CodeSet,
-          os.CountryCode,
-          os.CreationClassName,
-          os.CSCreationClassName,
-          os.CSDVersion,
-          os.CSName,
-          os.CurrentTimeZone,
-          os.Debug,
-          os.Description,
-          os.Distributed,
-          os.EncryptionLevel,
-          os.ForegroundApplicationBoost,
-          convert(os.FreePhysicalMemory),
-          convert(os.FreeSpaceInPagingFiles),
-          convert(os.FreeVirtualMemory),
-          parse_ms_date(os.InstallDate),
-          parse_ms_date(os.LastBootUpTime),
-          parse_ms_date(os.LocalDateTime),
-          os.Locale,
-          os.Manufacturer,
-          os.MaxNumberOfProcesses,
-          convert(os.MaxProcessMemorySize),
-          os.Name,
-          os.NumberOfLicensedUsers,
-          os.NumberOfProcesses,
-          os.NumberOfUsers,
-          os.Organization,
-          os.OSLanguage,
-          os.OSProductSuite,
-          os.OSType,
-          os.OtherTypeDescription,
-          os.PlusProductID,
-          os.PlusVersionNumber,
-          os.Primary,
-          os.ProductType,
-          os.respond_to?(:QuantumLength) ? os.QuantumLength : nil,
-          os.respond_to?(:QuantumType) ? os.QuantumType : nil,
-          os.RegisteredUser,
-          os.SerialNumber,
-          os.ServicePackMajorVersion,
-          os.ServicePackMinorVersion,
-          convert(os.SizeStoredInPagingFiles),
-          os.Status,
-          os.SuiteMask,
-          os.SystemDevice,
-          os.SystemDirectory,
-          os.SystemDrive,
-          convert(os.TotalSwapSpaceSize),
-          convert(os.TotalVirtualMemorySize),
-          convert(os.TotalVisibleMemorySize),
-          os.Version,
-          os.WindowsDirectory
+            host_os,
+            cpu_architecture,
+            `ver`.strip
         )
       end
     end
